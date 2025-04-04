f9433bc7004ad28450db966a31a0db8680a32f8c
Mina Lee <chsjen492@gmail.com>
2025-04-04 17:11:07 +0900

    Feat: Validation and Swagger settings

--- src/main.ts
+++ src/main.ts
@@ -1,8 +1,26 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
+import { ValidationPipe } from '@nestjs/common';
+import { DocumentBuilder, SwaggerModule } from '@nestjs/swagger';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
+
+  app.useGlobalPipes(
+    new ValidationPipe({
+      transform: true,
+      whitelist: true,
+    }),
+  );
+
+  const swaggerConfig = new DocumentBuilder()
+    .setTitle('REST Template')
+    .setDescription('Mash-Up Node팀 RESTful API 템플릿입니다.')
+    .build();
+
+  const swaggerDocument = () => SwaggerModule.createDocument(app, swaggerConfig);
+  SwaggerModule.setup('api-docs', app, swaggerDocument);
+
   await app.listen(process.env.PORT ?? 3000);
 }
 bootstrap();
