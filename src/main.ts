89945e6878bb2de16caadc6247512666608a38aa
Mina Lee <109032458+chsjen492@users.noreply.github.com>
2025-04-07 13:02:00 +0900

    Validation + Swagger 설정 (#2)
    * Feat: Validation and Swagger settings
    
    * refactor: 리뷰 반영
    
    ---------
    
    Co-authored-by: J-Hoplin <hoplin.dev@gmail.com>

--- src/main.ts
+++ src/main.ts
@@ -1,8 +1,14 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
+import { swaggerConfig } from "@/app.swagger";
+import { nestConfig } from "@/app.config";
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
+
+  nestConfig(app);
+  swaggerConfig(app);
+
   await app.listen(process.env.PORT ?? 3000);
 }
 bootstrap();
