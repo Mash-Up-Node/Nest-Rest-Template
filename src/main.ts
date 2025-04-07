f4af7d6c7553f752fe1a989d3de8d77f2e247321
Mina Lee <chsjen492@gmail.com>
2025-04-07 13:00:54 +0900

    refactor: 리뷰 반영

--- src/main.ts
+++ src/main.ts
@@ -1,25 +1,13 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
-import { ValidationPipe } from '@nestjs/common';
-import { DocumentBuilder, SwaggerModule } from '@nestjs/swagger';
+import { swaggerConfig } from "@/app.swagger";
+import { nestConfig } from "@/app.config";
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
 
-  app.useGlobalPipes(
-    new ValidationPipe({
-      transform: true,
-      whitelist: true,
-    }),
-  );
-
-  const swaggerConfig = new DocumentBuilder()
-    .setTitle('REST Template')
-    .setDescription('Mash-Up Node팀 RESTful API 템플릿입니다.')
-    .build();
-
-  const swaggerDocument = () => SwaggerModule.createDocument(app, swaggerConfig);
-  SwaggerModule.setup('api-docs', app, swaggerDocument);
+  nestConfig(app);
+  swaggerConfig(app);
 
   await app.listen(process.env.PORT ?? 3000);
 }
