687504f026e8e9be002c913b6867db63a5d5323e
Choi-JY1107 <cjy11230@kakao.com>
2025-04-13 22:05:26 +0900

    refactor: Object.freeze로 구현한 상수를 enum으로 변경

--- src/common/exception/error-message.enum.ts
+++ src/common/exception/error-message.enum.ts
@@ -1,9 +1,9 @@
-export const UserMessage = Object.freeze({
-    USER_NOT_FOUND: '사용자를 찾을 수 없습니다.',
-    USER_ALREADY_EXISTS: '이미 존재하는 사용자입니다.',
-    PASSWORD_INVALID: '비밀번호가 일치하지 않습니다.',
-} as const)
-
-export const AppMessage = Object.freeze({
-    TEST_EXCEPTION: '테스트용 예외입니다. 노드팀 화이팅!',
-} as const)
+export enum UserMessage {
+    USER_NOT_FOUND = '사용자를 찾을 수 없습니다.',
+    USER_ALREADY_EXISTS = '이미 존재하는 사용자입니다.',
+    PASSWORD_INVALID = '비밀번호가 일치하지 않습니다.',
+}
+  
+export enum AppMessage {
+    TEST_EXCEPTION = '테스트용 예외입니다. 노드팀 화이팅!',
+}
