bdc56d3836434248120e07431cbafc3573e3c07d
daisy-kim <sally0226@naver.com>
2025-04-20 22:33:06 +0900

    Refactor: apply GlobalAuthGuard
    - module 에 주입하는 방식으로 변경

--- src/app.module.ts
+++ src/app.module.ts
@@ -4,10 +4,17 @@ import { TypeOrmModule } from '@nestjs/typeorm';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { typeormConfig } from './common/database/config';
+import { GlobalAuthGuard } from './common/guard/global-auth.guard';
 
 @Module({
   imports: [TypeOrmModule.forRoot(typeormConfig), UsersModule],
   controllers: [AppController],
-  providers: [AppService],
+  providers: [
+    AppService,
+    {
+      provide: 'APP_GUARD',
+      useClass: GlobalAuthGuard,
+    },
+  ],
 })
 export class AppModule {}
--- src/main.ts
+++ src/main.ts
@@ -1,17 +1,13 @@
-import { NestFactory, Reflector } from '@nestjs/core';
+import { NestFactory } from '@nestjs/core';
 import { AppModule } from '@/app.module';
 import { swaggerConfig } from '@/app.swagger';
 import { nestConfig } from '@/app.config';
-import { GlobalAuthGuard } from '@/common/guard/global-auth.guard';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
 
   nestConfig(app);
   swaggerConfig(app);
-
-  const reflector = app.get(Reflector);
-  app.useGlobalGuards(new GlobalAuthGuard(reflector));
 
   await app.listen(process.env.PORT ?? 3000);
 }
